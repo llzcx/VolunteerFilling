c7df1cde8ecc8a1fc3ec7331cae6972d6feb0f66
cx <3207242935@qq.com>
2024-01-26 16:22:50 +0800

    fix: token

--- src/main/java/com/social/demo/controller/auth/TestController.java
+++ src/main/java/com/social/demo/controller/auth/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 用于测试RBAC权限
+ * 用于测试RBAC权限(验权功能开启需要联系后端)
  * @author 陈翔
  */
 @RestController
