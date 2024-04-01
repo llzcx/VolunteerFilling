e3356db22b7ba26ae83ae4ec917075a539e439ff
cx <3207242935@qq.com>
2024-04-01 18:07:24 +0800

    build(): JVM参数设置

--- src/main/bin/windows.bat
+++ src/main/bin/windows.bat
@@ -1,3 +1,3 @@
 @echo off
 chcp 65001 > nul
-java -Dfile.encoding=utf-8 -jar volunteer-filling.jar
+java -Dfile.encoding=utf-8 -jar -Xms512m -Xmx4096m -XX:ParallelGCThreads=4 -XX:+UseParallelGC volunteer-filling.jar
